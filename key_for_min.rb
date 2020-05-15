2dfd1d47b13e4f6e7a649ee79596fe07134cc5c0
Shakore McGill <mcgills2093@gmail.com>
2020-05-15 18:07:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   minumum_value = 0 
   name_hash.each do |key, value|
-    if minumum_value == 
+    if minumum_value == 0 || minumum_value < value
     
   end
 end
