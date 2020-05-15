ceb693c836cd740ed49052548d554d323dccee66
Shakore McGill <mcgills2093@gmail.com>
2020-05-15 18:10:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  minumum_key = nil
   minumum_value = 0 
   name_hash.each do |key, value|
     if minumum_value == 0 || value < minumum_value
